b39ff213059d26b7cec7f3350b5dc02dfdfe9a9b
fozzymo2 <danielcslattery@gmail.com>
2023-03-01 14:34:55 -0600

    re-did how a building's number of apartments is calculated to be simpler and more direct

--- PropertyManager/src/main/java/PropertyManager/Repositories/BuildingRepository.java
+++ PropertyManager/src/main/java/PropertyManager/Repositories/BuildingRepository.java
@@ -1,12 +1,10 @@
 package PropertyManager.Repositories;
 
 import PropertyManager.Model.Building;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface BuildingRepository extends CrudRepository<Building, Long> {
@@ -15,15 +13,4 @@ public interface BuildingRepository extends CrudRepository<Building, Long> {
             nativeQuery = true)
     public List<Building> findBuildingByAddress(@Param("address") String address);
 
-    // Directly adding or deleting from a field when the number of apartments changes may be
-    // easier but this uses the concept of GROUP BY and UPDATE, which is good to understand
-    @Modifying
-    @Transactional
-    @Query(value = "UPDATE Building AS d " +
-            "INNER JOIN" +
-            "   (SELECT a.building_id, COUNT(a.id) AS count FROM Apartment AS a " +
-            "    GROUP BY a.building_id) AS c " +
-            "ON d.id = c.building_id " +
-            "SET d.number_apartments = c.count;", nativeQuery = true)
-    public void updateNumberOfApartmentsInBuilding();
 }
